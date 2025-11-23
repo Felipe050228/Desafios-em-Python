1579612ef2fba9604795d80650a6bb12c9ecb5c3
Felipe Alencar Lima <felipe.alencarlima@gmail.com>
2025-11-22 21:27:01 -0300

    Add comments to number parity check

--- Nível_1/tr004.py
+++ Nível_1/tr004.py
@@ -1,5 +1,5 @@
-num =  int(input("Digite um numero: "))
-if num % 2 == 0:
-    print(f'O numero {num} é par')
-elif num % 2 == 1:
-    print(f'O numero {num} é impar')
+num =  int(input("Digite um numero: ")) # ler um número 
+if num % 2 == 0: # verificar se o número é par
+    print(f'O numero {num} é par') # confirma que é par
+elif num % 2 == 1: # verificar se o número é impar
+    print(f'O numero {num} é impar') # confirma que é impar
