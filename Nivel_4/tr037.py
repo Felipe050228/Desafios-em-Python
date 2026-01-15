bbdc9d2af4ed2cfbbe92d5db618d29faa5da1994
Felipe Alencar Lima <felipe.alencarlima@gmail.com>
2026-01-15 20:41:11 -0300

    Add files via upload

---
+++ Nivel_4/tr037.py
@@ -0,0 +1,8 @@
+class comprimentos:
+    @staticmethod
+    def saudacao(nome):
+        return f'\nbom dia {nome}, como esta? \n'
+    
+
+converca = comprimentos.saudacao('Felipe')
+print(converca)
